76274fd669303f0cef1a12f1496bbe070ad1c77d
Brady Shutt <ubuntu@ip-172-31-10-167.us-west-1.compute.internal>
2016-10-13 04:51:51 +0000

    Integrated temp landing page

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const http = require('http')
+const fs = require('fs');
 
 let server = http.createServer((req, res) => {
    console.log(req.url)
-   res.end('Coplan!')
+   fs.readFile('web/index.html', (err, data) => {
+      if (err) console.error(err)
+      else {
+         res.writeHeader(200, {'ContentType': 'text/html'});
+         res.end(data) 
+      }
+   })
 })
 
 server.listen(80)
---
+++ startServer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo nodejs server.js
