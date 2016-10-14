dbc612a1e315ea4a4963129075496799bfd3e66c
Brady <bradyshutt@gmail.com>
2016-10-14 14:57:57 -0700

    Updated to a sensical file tree structure...

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const http = require('http')
 const fs = require('fs');
 const conf = require('./config.js')
 
-
 let server = http.createServer((req, res) => {
    console.log(req.url)
    fs.readFile('web/index.html', (err, data) => {
